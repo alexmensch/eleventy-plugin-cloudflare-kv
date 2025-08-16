d02640b1734f34921982f80cd682a52d3b96aa9a
Alex Marshall <git@alxm.me>
2025-08-16 13:03:42 +0100

    Config files

---
+++ eslint.config.js
@@ -0,0 +1,48 @@
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        console: "readonly",
+        process: "readonly",
+        fetch: "readonly"
+      }
+    },
+    rules: {
+      // Possible Errors
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+
+      // Best Practices
+      eqeqeq: "error",
+      "no-eval": "error",
+      "no-implied-eval": "error",
+
+      // Stylistic Issues
+      indent: ["error", 2],
+      quotes: ["error", "double"],
+      semi: ["error", "always"],
+      "comma-dangle": ["error", "never"],
+      "no-trailing-spaces": "error",
+      "eol-last": "error"
+    }
+  },
+  {
+    files: ["test/**/*.js"],
+    languageOptions: {
+      globals: {
+        describe: "readonly",
+        it: "readonly",
+        before: "readonly",
+        after: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly"
+      }
+    }
+  }
+];
